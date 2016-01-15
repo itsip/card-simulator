9549c43e0d4ba562b70fd94d14c62fe16e6f2bfb
Ian Paterson <ianhpaterson@gmail.com>
2016-01-14 22:36:40 -0500

    started on game of war

--- CardSimulator/WarSimulation.cpp
+++ CardSimulator/WarSimulation.cpp
@@ -8,12 +8,13 @@ using namespace std;
 int main() {
 	srand(time(0));
 
-	Deck mainDeck(1);
+	Deck mainDeck(2);
 	Deck hand1;
 	Deck hand2;
 
 	mainDeck.shuffle();
 	while (mainDeck.getNumOfCards()) {
-
+		hand1.addCard(mainDeck.takeTopCard());
+		hand2.addCard(mainDeck.takeTopCard());
 	}
 }
